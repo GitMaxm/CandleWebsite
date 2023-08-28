0c2ea5095ba2806acfc525fa7fca606486679be7
Ivan <ivanenkomax@rambler.ru>
2023-08-28 21:02:18 +0300

    feedback адаптив и слайдер

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@ const menuMobile = document.querySelector('.header__menu-list');
 menuBtn.addEventListener('click', function (e) {
     menuMobile.classList.toggle('menu__open')
 });
+
+const swiperOne = new Swiper('.feedback__slider', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
