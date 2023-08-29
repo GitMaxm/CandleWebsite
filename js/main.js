3370bfc1578006d1c93e7d6ef9f8844cc3860257
Ivan <ivanenkomax@rambler.ru>
2023-08-29 19:35:17 +0300

    certificates feedback адаптив

--- js/main.js
+++ js/main.js
@@ -14,9 +14,30 @@ const swiperOne = new Swiper('.feedback__slider', {
 
 const swiperTwo = new Swiper('.certificates__slider', {
     loop: true,
-    slidesPerView: 3,
-    spaceBetween: 20,
+
     pagination: {
         el: '.swiper-pagination',
+    },
+
+    breakpoints: {
+        2560: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1000: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        480: {
+            slidesPerView: 2,
+        },
+        410: {
+            slidesPerView: 1,
+        },
+        360: {
+            slidesPerView: 1,
+        },
     },
 });
